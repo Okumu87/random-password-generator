9a973e5c8c2274020794e76dd22a4ff707a5f918
okumu87 <okumu793@gmail.com>
2026-05-13 22:00:37 +0200

    modified  code

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-const btnElement = document.querySelector('.btn');
+const btnEl = document.querySelector('.btn');
 const inputElement = document.querySelector('#input');
 const copyElement = document.querySelector('.fa-copy');
 const alertContainer = document.querySelector('.alert-container');
 
-btnElement.addEventListener('click', () => {
-   createPassword();
-});
 
-copyElement.addEventListener('click', () => {
-
-    if (inputElement.value !== '') {
-   inputElement.select();
-   document.execCommand('copy');
-    } 
-});
+btnEl.addEventListener('click', createPassword);
 
 function createPassword() {
-   const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-   const passwordLength = 12;
-   let password = '';
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const passwordLength = 12;
+    let password = '';
 
-   for (let i = 0; i < passwordLength; i++) {
-      const randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-   }
-   inputElement.value = password;
-   alertContainer.innerText = `${password} Copied`;
-   alertContainer.classList.add('active');
-   setTimeout(() => {
-      alertContainer.classList.remove('active');
-   }, 2000);
-}   
+    for (let i = 0; i < passwordLength; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    inputElement.value = password;
+}
 
+copyElement.addEventListener('click', () => {
+    if (inputElement.value) {
+        inputElement.select();
+        document.execCommand('copy');
+        showAlert('Password copied to clipboard!');
+    } else {
+        showAlert('No password to copy!');
+    }
+});
+
+function showAlert(message) {
+    alertContainer.textContent = message;
+    alertContainer.style.display = 'block';
+    setTimeout(() => {
+        alertContainer.style.display = 'none';
+    }, 2000);
+}
